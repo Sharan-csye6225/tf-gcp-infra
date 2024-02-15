5430600c7cdbe009e435bb22f2147af256358f70
SharanKumarPrabhakaran <prabhakaran.sh@northeastern.edu>
2024-02-14 22:59:03 -0500

    Added files to gitignore and updated tf files

---
+++ main.tf
@@ -0,0 +1,34 @@
+# Terraform Configures the Google Cloud Provider(GCP)
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+# Create Virtual Private Cloud (VPC)
+resource "google_compute_network" "my_vpc_test" {
+  name                            = var.vpc_name
+  auto_create_subnetworks         = var.auto_create_subnetworks
+  routing_mode                    = var.routing_mode
+  delete_default_routes_on_create = var.delete_default_routes_on_create
+}
+
+# Create Route Table
+resource "google_compute_route" "webapp_route" {
+  name             = "${var.vpc_name}-route-table"
+  network          = google_compute_network.my_vpc_test.self_link
+  dest_range       = var.dest_range
+  next_hop_gateway = var.next_hop_gateway
+  priority         = 1000
+  depends_on       = [google_compute_subnetwork.subnet["webapp"]]
+}
+
+# Create Subnets dynamically
+resource "google_compute_subnetwork" "subnet" {
+  for_each = var.subnet_map
+
+  name          = "${var.vpc_name}-${each.key}"
+  ip_cidr_range = each.value
+  network       = google_compute_network.my_vpc_test.self_link
+}
+
