02ebc123f1386700475c8ffdcf329f54ecfaea8a
SharanKumarPrabhakaran <prabhakaran.sh@northeastern.edu>
2024-02-21 19:41:09 -0500

    Initial commit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Terraform Configures the Google Cloud Provider(GCP)
+# Terraform Configures the Google Cloud Provider (GCP)
 provider "google" {
   credentials = file(var.credentials_file)
   project     = var.project_id
@@ -6,7 +6,7 @@ provider "google" {
 }
 
 # Create Virtual Private Cloud (VPC)
-resource "google_compute_network" "my_vpc_test" {
+resource "google_compute_network" "my_vpc" {
   name                            = var.vpc_name
   auto_create_subnetworks         = var.auto_create_subnetworks
   routing_mode                    = var.routing_mode
@@ -16,7 +16,7 @@ resource "google_compute_network" "my_vpc_test" {
 # Create Route Table
 resource "google_compute_route" "webapp_route" {
   name             = "${var.vpc_name}-route-table"
-  network          = google_compute_network.my_vpc_test.self_link
+  network          = google_compute_network.my_vpc.self_link
   dest_range       = var.dest_range
   next_hop_gateway = var.next_hop_gateway
   priority         = 1000
@@ -29,6 +29,63 @@ resource "google_compute_subnetwork" "subnet" {
 
   name          = "${var.vpc_name}-${each.key}"
   ip_cidr_range = each.value
-  network       = google_compute_network.my_vpc_test.self_link
+  network       = google_compute_network.my_vpc.self_link
 }
 
+# Create Subnets Firewall rule - Deny all ports
+resource "google_compute_firewall" "firewall_deny" {
+  name        = var.firewall_deny_name
+  network     = google_compute_network.my_vpc.self_link
+  description = var.firewall_description
+
+  deny {
+    protocol = var.firewall_deny_protocol
+    ports    = var.firewall_deny_port
+  }
+
+  priority      = var.firewall-deny-priority
+  source_ranges = var.source_ranges
+  target_tags   = var.target_tags
+}
+
+# Create Subnets Firewall rule - Allow http 8080
+resource "google_compute_firewall" "firewall_allow" {
+  name        = var.firewall_allow_name
+  network     = google_compute_network.my_vpc.self_link
+  description = var.firewall_description
+
+  allow {
+    protocol = var.firewall_allow_protocol
+    ports    = var.firewall_allow_port
+  }
+
+  priority      = var.firewall-allow-priority
+  source_ranges = var.source_ranges
+  target_tags   = var.target_tags
+}
+
+# Create GCP Compute Engine Instance
+resource "google_compute_instance" "my_vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = var.tags
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet["webapp"].self_link
+    network    = google_compute_network.my_vpc.self_link
+    access_config { #Generates a external IP address
+      nat_ip       = var.access_config_nat_ip
+      network_tier = var.access_config_network_tier
+    }
+  }
+
+  boot_disk {
+    auto_delete = var.boot_disk_auto_delete
+    initialize_params {
+      image = var.custom-image-name
+      size  = var.boot_disk_size
+      type  = var.boot_disk_type
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -48,3 +48,108 @@ variable "next_hop_gateway" {
   type        = string
 }
 
+variable "zone" {
+  description = "Zone for the VM."
+  type        = string
+}
+
+variable "firewall_allow_port" {
+  description = "Firewall Port number."
+  type        = list(string)
+}
+
+variable "firewall_deny_port" {
+  description = "Firewall Port number."
+  type        = list(string)
+}
+
+variable "firewall_allow_protocol" {
+  description = "Firewall allow protocol."
+  type        = string
+}
+
+variable "firewall_deny_protocol" {
+  description = "Firewall deny protocol."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags."
+  type        = list(string)
+}
+
+variable "target_tags" {
+  description = "Target tags."
+  type        = list(string)
+}
+
+variable "firewall_description" {
+  description = "Description for the Firewall "
+  type        = string
+}
+
+variable "custom-image-name" {
+  description = "Description for the Firewall "
+  type        = string
+}
+
+variable "firewall-allow-priority" {
+  description = "Description for the Firewall rule priority "
+  type        = number
+}
+
+variable "firewall-deny-priority" {
+  description = "Description for the Firewall rule priority "
+  type        = number
+}
+
+variable "source_ranges" {
+  description = "Description for the Firewall rule priority "
+  type        = list(string)
+}
+
+variable "vm_name" {
+  description = "Description for name of the VM "
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Description for machine type "
+  type        = string
+}
+
+variable "access_config_nat_ip" {
+  description = "Description for nat ip"
+  type        = string
+}
+
+variable "access_config_network_tier" {
+  description = "Description for network tier"
+  type        = string
+}
+
+variable "boot_disk_size" {
+  description = "Description for boot disk size"
+  type        = number
+}
+
+variable "boot_disk_type" {
+  description = "Description for boot disk type"
+  type        = string
+}
+
+variable "boot_disk_auto_delete" {
+  description = "Description for boot disk auto delete"
+  type        = bool
+}
+
+variable "firewall_deny_name" {
+  description = "Description for firewall deny name"
+  type        = string
+}
+
+variable "firewall_allow_name" {
+  description = "Description for firewall deny name"
+  type        = string
+}
+
